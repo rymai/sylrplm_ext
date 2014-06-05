9e843e33435e7b8cc2de3035f49d8b8c00783455
sylvani <sylvani@laposte.net>
2014-06-05 22:20:53 +0200

    debug file_utils

--- lib/sylrplm_ext/file_utils.rb
+++ lib/sylrplm_ext/file_utils.rb
@@ -1,8 +1,14 @@
 module FileUtils
 	def self.sync_file file_in , file_out
-  	unless FileUtils.compare_file file_in , file_out
+		if File.exists? file_out
+  		equal = FileUtils.compare_file file_in , file_out
+  	else
+  		equal = false
+  	end
+  	unless equal
   		puts fname="#{__FILE__}: copy_file #{file_in} , #{file_out}"
   		FileUtils.copy_file file_in , file_out
   	end
+  
   end
 end
